6b8ce6d37a058e27f34f2017b67582cd0bb577de
Chomitzki <chomitzk@vdi-sccubu-013.lancs.local>
2021-10-17 23:56:15 +0100

    food drawn in different colors@

--- src/main/java/de/mat2095/my_slither/Food.java
+++ src/main/java/de/mat2095/my_slither/Food.java
@@ -1,12 +1,18 @@
 package de.mat2095.my_slither;
 
+import java.awt.Color;
+import java.util.Random;
+
 
 class Food {
+
+    private static final Color FOOD_COLORS[] = new Color[]{new Color(0xCC7832), new Color(0xAA0012), new Color(0x106A0)};
 
     final int x, y;
     private final double size;
     private final double rsp;
     private final long spawnTime;
+    private Color col;
 
     Food(int x, int y, double size, boolean fastSpawn) {
         this.x = x;
@@ -14,6 +20,8 @@ class Food {
         this.size = size;
         this.rsp = fastSpawn ? 4 : 1;
         spawnTime = System.currentTimeMillis();
+        this.col = FOOD_COLORS[new Random().nextInt(FOOD_COLORS.length)];
+
     }
 
     double getSize() {
@@ -28,4 +36,10 @@ class Food {
             return (1 - Math.cos(Math.PI * fillRate)) / 2 * size;
         }
     }
+
+    /**
+     * returns the color of the food.
+     * @return //the color of the food.
+     */
+    Color getCol() { return col; }
 }
